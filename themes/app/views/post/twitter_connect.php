79d1e2198bee1305727e9eb4843c79dbf859973e
Aaron Parecki <aaron@parecki.com>
2011-01-16 21:09:28 -0800

    Basic theming of the in-app screens to include the background texture that's in the iPhone app.

--- themes/app/views/post/twitter_connect.php
+++ themes/app/views/post/twitter_connect.php
@@ -1,4 +1,4 @@
 
-<div style="font-size: 14pt; width: 260px; margin: 0 auto; text-align: center;">
-	You're all set! Close this screen to go back to the app and click "Twitter" again!
+<div style="font-size: 14pt; width: 290px; margin: 40px auto; text-align: center;">
+	You're all set! Close this screen and click the Twitter button again!
 </div>
