fad2aa41b2dd2701242067fb2ba3ad5ceb913de9
Aaron Parecki <aaron@parecki.com>
2011-01-30 15:25:07 -0800

    Updates title tag and description.

--- themes/standard/views/layouts/header.php
+++ themes/standard/views/layouts/header.php
@@ -3,7 +3,9 @@
 <head>
 	<title><?=$title?></title>
 	<link rel="stylesheet" href="<?=$theme_root?>styles.css" type="text/css" />
-	<meta name="description" content="An open source website and mobile app for securely sharing location data with GPS and SMS" />
+	<meta name="description" content="A secure, real-time mobile and web platform for location sharing." />
+	<meta name="og:description" content="A secure, real-time mobile and web platform for location sharing." />
+	<meta name="og:title" content="Geoloqi" />
 	<script type="text/javascript" src="<?=$theme_root?>jquery-1.4.2.min.js"></script>
 	<script type="text/javascript" src="<?=$theme_root?>geoloqi.js"></script>
 <?php 
